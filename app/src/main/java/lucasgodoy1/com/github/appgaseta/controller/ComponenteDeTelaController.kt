65742efc8167eee5ec1f1766db7d3fff1e5e613d
Lucas Godoy <lucassdegodoy@hotmail.com>
2024-11-17 17:24:40 -0300

    feat: método salvar

--- app/src/main/java/lucasgodoy1/com/github/appgaseta/controller/ComponenteDeTelaController.kt
+++ app/src/main/java/lucasgodoy1/com/github/appgaseta/controller/ComponenteDeTelaController.kt
@@ -1,5 +1,6 @@
 package lucasgodoy1.com.github.appgaseta.controller
 
+import android.content.SharedPreferences
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -8,17 +9,20 @@ import androidx.appcompat.widget.AppCompatButton
 import lucasgodoy1.com.github.appgaseta.R
 import lucasgodoy1.com.github.appgaseta.util.UtilGasEta
 import lucasgodoy1.com.github.appgaseta.view.GasEtaActivity
+import java.time.LocalDateTime
 
 class ComponenteDeTelaController(private val gasEtaActivity: GasEtaActivity) {
 
     private val idBtnFinalizar: AppCompatButton = gasEtaActivity.findViewById(R.id.idAndroidBtnFinalizar)
     private val idBtnLimpar: AppCompatButton = gasEtaActivity.findViewById(R.id.idAndroidBtnLimp)
     private val idBtnCalcular : AppCompatButton = gasEtaActivity.findViewById(R.id.idAndroidBtnCalcular)
+    private val idBtnSalvar : AppCompatButton = gasEtaActivity.findViewById(R.id.idAndroidBtnSal)
 
     private var idEtanol: EditText = gasEtaActivity.findViewById(R.id.idTxtEtanol)
     private var idGasolina : EditText = gasEtaActivity.findViewById(R.id.idTxtGasolina)
     private var idResultado : TextView = gasEtaActivity.findViewById(R.id.idAndroidResultado)
 
+    private var sharedPreferences : SharedPreferences = gasEtaActivity.getSharedPreferences("calculated_save", 0)
 
     private fun calcular(){
         idBtnCalcular.setOnClickListener(View.OnClickListener{
@@ -41,6 +45,18 @@ class ComponenteDeTelaController(private val gasEtaActivity: GasEtaActivity) {
         })
     }
 
+    private fun salvar(){
+        idBtnSalvar.setOnClickListener(View.OnClickListener {
+            var gasEta = sharedPreferences.edit()
+            gasEta.putString("valor_gasolina", idGasolina.text.toString())
+            gasEta.putString("valor_etanol", idEtanol.text.toString())
+            gasEta.putString("resultado_calculo", idResultado.text.toString())
+            gasEta.putString("data", LocalDateTime.now().toString())
+
+            gasEta.apply()
+            Toast.makeText(gasEtaActivity, "Salvo com Sucesso!", Toast.LENGTH_LONG).show()
+        })
+    }
 
     private fun finalizar(){
         idBtnFinalizar.setOnClickListener(View.OnClickListener {
@@ -52,6 +68,7 @@ class ComponenteDeTelaController(private val gasEtaActivity: GasEtaActivity) {
     fun iniciarBotoes(){
         calcular()
         limpar()
+        salvar()
         finalizar()
     }
 
